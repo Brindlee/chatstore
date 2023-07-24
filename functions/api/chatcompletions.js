385cea1ff72c59ec2af547db46acd3bd63321e69
sampada@dreamwares.com <sampada@dreamwares.com>
2023-07-25 03:40:23 +0530

    Updated return statement7

--- functions/api/chatcompletions.js
+++ functions/api/chatcompletions.js
@@ -13,7 +13,7 @@ const getData = async (data,env) => {
   
 	try {
 	  const result = await fetch(url, options);
-	  console.log("result", await result.text()      );
+	 // console.log("result", await result.text()      );
 	  return result;
 	} catch (e) {
 	  console.log(e);
@@ -31,7 +31,7 @@ console.log(' body 1',reqbody);
     var reqData = JSON.stringify({model:reqbody.model, messages : reqbody.messages});
 	try {
 	  const resp = await getData(reqData,env);
-	  console.log('resp: ', JSON.stringify(await resp.json()));
+	  console.log('resp: ', resp.json());
 	  //return new Response(JSON.stringify(resp.json()));
 	  return new Response('test');
 	  //res.send(resp.data);

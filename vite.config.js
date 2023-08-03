4cc44cbf654a8f3c81912f5598c429343ec53ab7
sampada@dreamwares.com <sampada@dreamwares.com>
2023-08-04 04:23:32 +0530

    editor import issue3

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
     rollupOptions: {
         // make sure to externalize deps that shouldn't be bundled
         // into your library
-        external: ['vue3-ace-editor'],
+        external: ['vue3-ace-editor', 'ace-builds'],
         /*output: {
             // Provide global variables to use in the UMD build
             // for externalized deps
